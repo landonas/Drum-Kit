83221d0c7f51176a61aa25ffc0171e746778616d
landonas@hawaii.edu <landonas@hawaii.edu>
2018-07-12 15:39:07 -1000

    created a new JS file

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
+  
+    audio.play();
+  });
+   
+</script>
