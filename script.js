63d03866b1037240a99ad19049b6ba340998aacd
landonas@hawaii.edu <landonas@hawaii.edu>
2018-07-16 12:43:02 -1000

    change in values

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
   window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     audio.currentTime = 0;
+   
   
     audio.play();
+    key.classlist.add('playing');
+    const key = document.querySelector('.key'); 
   });
    
