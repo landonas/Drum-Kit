16c5599ac50bc84a5f28763e5bd8353806a35dac
landonas@hawaii.edu <landonas@hawaii.edu>
2018-07-15 23:09:54 -1000

    resolved issue with sound not playing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script>
+
   window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     audio.currentTime = 0;
@@ -6,4 +6,3 @@
     audio.play();
   });
    
-</script>
